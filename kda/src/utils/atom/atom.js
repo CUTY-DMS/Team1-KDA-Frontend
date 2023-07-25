e38272df1a797dff96245aa41649b4a915860e06
dutexion <dutexion@gmail.com>
2023-07-26 00:41:06 +0900

    🌟 :: 전역 상태 추가

--- kda/src/utils/atom/atom.js
+++ kda/src/utils/atom/atom.js
@@ -1,6 +1,11 @@
 import { atom } from "recoil";
 
 export const modalState = atom({
-    key: "modalState",
-    default: "none"
+  key: "modalState",
+  default: "none",
+});
+
+export const studentEmail = atom({
+  key: "studentEmail",
+  default: "none",
 });
