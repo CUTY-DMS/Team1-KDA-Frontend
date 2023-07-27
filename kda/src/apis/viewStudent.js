e99d94c9e5f5eebc9b87b03bd3ccb108ce2adebe
dutexion <dutexion@gmail.com>
2023-07-27 14:03:47 +0900

    🌧 :: 학생 전체 보기 서버 연동 진짜 완성

--- kda/src/apis/viewStudent.js
+++ kda/src/apis/viewStudent.js
@@ -1,16 +1,10 @@
 import axios from "axios";
-import { baseURL } from "./baseURL";
-import { json } from "react-router-dom";
 
-export const viewStudent = async (data) => {
-    const {a} = baseURL; 
-    const response = await axios.get(`${a}/admin/student/list`, {
-        params: {
-            gradeClass:`${data}`
-        },
-        headers: {
-            Authorization:"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxYWRtaW4uY29tIiwicm9sZXMiOiJST0xFX0FETUlOIiwiaWF0IjoxNjg5ODM2NjY5LCJleHAiOjE2ODk4Mzg0Njl9.m7VsVCAzR_COCaGO0aXkEEfmM9xf_-49o3JaVS1ZYnA"
-        },
-    });
-    return response;
+export const viewStudent = async (accessToken) => {
+  const response = await axios.get(`/admin/student/list`, {
+    headers: {
+      Authorization: accessToken,
+    },
+  });
+  return response;
 };
