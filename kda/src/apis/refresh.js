1df313e1e3d58971213ab197c60fddff8222e6fa
dutexion <dutexion@gmail.com>
2023-07-28 21:03:32 +0900

    🌧 :: 만료된 토큰 갱신

--- kda/src/apis/refresh.js
+++ kda/src/apis/refresh.js
@@ -2,11 +2,19 @@ import axios from "axios";
 
 export const refresh = async () => {
   const token = localStorage.getItem("refreshToken");
+  const CAToken = localStorage.getItem("accessToken");
   console.log(token);
   const response = await axios.post("/reissue", null, {
     headers: {
       AUTHORIZATION_HEADER: token,
     },
   });
-  return response;
+  const NAToken = response.data.accessToken;
+  if (CAToken == NAToken) {
+    return;
+  } else {
+    localStorage.removeItem("accessToken");
+    localStorage.setItem("accessToken", NAToken);
+    window.location.reload();
+  }
 };
